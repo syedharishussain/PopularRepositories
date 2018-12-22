13fd4b35a5591d8354a83d5caf743e98774fbfb0
syedharishussain <syedharishussain@gmail.com>
2018-12-23 02:42:40 +0500

    corrected Info.plist location in project folder

--- PopularRepositories/Constants/AppConstants.swift
+++ PopularRepositories/Constants/AppConstants.swift
@@ -11,6 +11,7 @@ import Alamofire
 
 struct AppConstants {
     static let apiUrl: String = "https://api.github.com/search/repositories?q=stars%3A%3E0&s=stars&type=Repositories"
+    
     static let decoder: JSONDecoder = {
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
